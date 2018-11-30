667d97a1acda62b435c6042a4c6c7aad56364915
italo luiz <italo.luis.s@gmail.com>
2018-11-30 11:26:45 -0200

    tirar alguns dados

--- index_api.php
+++ index_api.php
@@ -4,10 +4,10 @@ include 'vendor/autoload.php';
 
 
 $base_url = 'http://localhost/faceads/';
-$app_id = '2204624902915529';
-$app_secret = '0a965d1a202b430d2d5aba520e177618';
-$access_token = 'EAAfVGDwzhckBAD2royRGO1Lnls4ds7ESHnVUZAtMQKbfBilCk3YbxkDqsRCHkCw38VmyU6y2mBntZA8tIV8sQSuE3dfBVISiwq9cL20WnFjPIiC3k56afvuSPkVF4yc8lJqD02q9ArxadaqGudkD1O5lrIwkf8FLzmswQ1I6p0SVfQlyP7';
-$account_id = 'act_258725951253917';
+$app_id = '';
+$app_secret = '';
+$access_token = '';
+$account_id = '';
 
 use FacebookAds\Api;
 use FacebookAds\Object\AdAccount;
