26cec758789806c7fba5c305f4201e8be4e890fc
italo luiz <italo.luis.s@gmail.com>
2018-11-30 11:24:36 -0200

    tirar alguns dados

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
 /*
 $base_url = 'http://localhost/faceads/';
-$app_id = '2204624902915529';
-$app_secret = '0a965d1a202b430d2d5aba520e177618';
-$access_token = 'EAAfVGDwzhckBAD2royRGO1Lnls4ds7ESHnVUZAtMQKbfBilCk3YbxkDqsRCHkCw38VmyU6y2mBntZA8tIV8sQSuE3dfBVISiwq9cL20WnFjPIiC3k56afvuSPkVF4yc8lJqD02q9ArxadaqGudkD1O5lrIwkf8FLzmswQ1I6p0SVfQlyP7';
+$app_id = '';
+$app_secret = '';
+$access_token = '';
 */
 
 $query_config = QB::table('tbl_configs');
